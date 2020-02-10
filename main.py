fd6fd0015707fae91af93b657f7128eaed55b60d
kryksyh <kryksyh@gmail.com>
2020-02-10 09:46:33 +0100

    args parsing fixed

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ async def my_event_handler(event):
         print("Command received")
         command, *args = event.raw_text.split(':')
         command = command.strip()
-        args = "".join().strip()
+        args = "".join(args).strip()
         if command == "set source by url":
             print("Updating source dialog to: ", args)
             await update_source_channel(args)
